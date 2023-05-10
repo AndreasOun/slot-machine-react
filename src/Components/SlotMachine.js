e1f0dc6dddb65cc61eb0614f37f5a01f3f9be2d6
AndreasOun <andreastthk@gmail.com>
2023-05-10 04:15:25 +0300

    Fix winning odds

--- src/Components/SlotMachine.js
+++ src/Components/SlotMachine.js
@@ -2,72 +2,138 @@ import React, { useState, useEffect } from 'react';
 import { Random } from 'random';
 import './SlotMachine.css';
 
+const SYMBOLS = ['🍒', '🍊', '🍇', '🍓','🍉', '🍌'];
+const WINNINGS_MAP = {
+  '🍒': 5,
+  '🍊': 20,
+  '🍇': 40,
+  '🍓': 60,
+  '🍉': 75,
+  '🍌': 100
+};
+
 function SlotMachine() {
   const [reels, setReels] = useState([]);
   const [credits, setCredits] = useState(100);
+  const [winning, setWinning] = useState(null);
+  const [totalWinnings, setTotalWinnings] = useState(0);
+  const [isSpinning, setIsSpinning] = useState(false);
   const randomInstance = new Random();
 
   useEffect(() => {
-    // Generate random symbols for each reel
+    resetReels();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function resetReels() {
     const newReels = [];
     for (let i = 0; i < 4; i++) {
       const newSymbols = [];
       for (let j = 0; j < 5; j++) {
-        const randomIndex = randomInstance.integer(0, 3);
-        const symbol = ['🍒', '🍊', '🍇', '🍓'][randomIndex];
+        const randomIndex = randomInstance.integer(0, SYMBOLS.length - 1);
+        const symbol = SYMBOLS[randomIndex];
+        newSymbols.push(symbol);
+      }
+      newReels.push(newSymbols);
+    }
+    setReels(newReels);
+    setWinning(null);
+  }
+
+  function spinReels() {
+    if (isSpinning) return;
+
+    setIsSpinning(true);
+
+    const newReels = [];
+    for (let i = 0; i < reels.length; i++) {
+      const newSymbols = [];
+      for (let j = 0; j < reels[i].length; j++) {
+        const randomIndex = randomInstance.integer(0, SYMBOLS.length - 1);
+        const symbol = SYMBOLS[randomIndex];
         newSymbols.push(symbol);
       }
       newReels.push(newSymbols);
     }
 
-    // Set the initial state for the reels
-    setReels(newReels);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // Empty array as second argument to run the effect only once on initial render
+    let totalWinnings = 0;
+    let winningSymbol = null;
 
-  const [isSpinning, setIsSpinning] = useState(false); // update state variable
 
-function spinReels() {
-  setIsSpinning(true); // set isSpinning to true when the button is clicked
-
-  // Generate random symbols for each reel
-  const newReels = [];
-  for (let i = 0; i < reels.length; i++) {
-    const newSymbols = [];
-    for (let j = 0; j < reels[i].length; j++) {
-      const randomIndex = randomInstance.integer(0, 3);
-      const symbol = ['🍒', '🍊', '🍇', '🍓'][randomIndex];
-      newSymbols.push(symbol);
+    // Check for winning combinations in columns
+    for (let i = 0; i < reels.length; i++) {
+      let currentSymbol = reels[0][i];
+      let currentCount = 1;
+      for (let j = 1; j < reels.length; j++) {
+        if (reels[j][i] === currentSymbol) {
+          currentCount++;
+        } else {
+          break;
+        }
+      }
+      if (currentCount === 4) {
+        const winnings = WINNINGS_MAP[currentSymbol];
+        totalWinnings += winnings;
+        winningSymbol = currentSymbol;
+      }
     }
-    newReels.push(newSymbols);
+
+
+    // Check for winning combinations in rows
+    for (let i = 0; i < reels.length; i++) {
+      let currentSymbol = reels[i][0];
+      let currentCount = 1;
+      for (let j = 1; j < reels[i].length; j++) {
+        if (reels[i][j] === currentSymbol) {
+          currentCount++;
+        } else {
+          break;
+        }
+      }
+      if (currentCount === 4) {
+        const winnings = WINNINGS_MAP[currentSymbol];
+        totalWinnings += winnings;
+        winningSymbol = currentSymbol;
+      }
+    }
+
+
+    setReels(newReels);
+    setCredits((credits) => credits - 1 + totalWinnings);
+    setWinning(winningSymbol);
+    setTotalWinnings(totalWinnings);
+
+    setTimeout(() => {
+      setIsSpinning(false);
+    }, 2000);
   }
 
-  // Update the state variables with the new symbols
-  setReels(newReels);
-  // Deduct credits from the user's balance
-  setCredits(credits - 1);
-
-  // set isSpinning back to false after 2 seconds to stop the animation
-  setTimeout(() => {
-    setIsSpinning(false);
-  }, 2000);
-}
 
   return (
-     <div className="slot-machine">
-         {/* Reel elements with a class based on isSpinning state */}
-         {reels.map((row, rowIndex) => (
-        <div key={rowIndex} className={`row ${isSpinning ? 'spin' : ''}`}>
-        {row.map((symbol, columnIndex) => (
-         <div key={columnIndex} className="symbol">
-          {symbol}
-         </div>
-     ))}
+    <div className="slot-machine">
+      {/* Reel elements with a class based on isSpinning state */}
+      {reels.map((row, rowIndex) => (
+        <div key={rowIndex} className={`row ${isSpinning ? "spin" : ""}`}>
+          {row.map((symbol, columnIndex) => (
+            <div
+              key={columnIndex}
+              className={`symbol ${symbol === winning ? "winner" : ""}`}
+            >
+              {symbol}
+            </div>
+          ))}
         </div>
-    ))}
-      <button onClick={spinReels}>Spin</button>
+      ))}
+      <button onClick={spinReels} disabled={isSpinning}>
+        {isSpinning ? "Spinning..." : "Spin"}
+      </button>
       {/* UI for displaying the user's credits */}
       <p>Credits: {credits}</p>
+      {winning && (
+        <div className="win-message">
+          <p>You won {totalWinnings} credits with {winning}!</p>
+        </div>
+      )}
     </div>
   );
 }
